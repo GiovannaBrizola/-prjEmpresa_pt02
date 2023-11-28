492b4775a5601c9cc68b798277fe957b20e2b877
GiovannaBrizola <133990989+GiovannaBrizola@users.noreply.github.com>
2023-11-28 14:26:04 -0300

    Add files via upload

---
+++ prjEmpresa/src/main/java/com/giovanna/prjEmpresa/entities/Funcionario.java
@@ -0,0 +1,92 @@
+package com.giovanna.prjEmpresa.entities;
+
+import java.time.LocalDate;
+
+import org.springframework.format.annotation.DateTimeFormat;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+
+@Entity
+@Table(name = "tb_funcionario")
+public class Funcionario {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long funcodigo;
+
+	private String funnome;
+
+
+	@DateTimeFormat(pattern = "dd/MM/yyyy")
+	private LocalDate funnascimento;
+
+	public void setFunsalario(long funsalario) {
+		this.funsalario = funsalario;
+	}
+
+
+	private long funsalario;
+
+	@ManyToOne
+	private Departamento departamento;
+
+	public Funcionario() {
+
+	}
+
+	public Funcionario(Long funcodigo, String funnome, LocalDate funnascimento, Long funsalario) {
+		super();
+		this.funcodigo = funcodigo;
+		this.funnome = funnome;
+		this.funnascimento = funnascimento;
+		this.funsalario = funsalario;
+	}
+
+	public Long getFuncodigo() {
+		return funcodigo;
+	}
+
+	public void setFuncodigo(Long funcodigo) {
+		this.funcodigo = funcodigo;
+	}
+
+	public String getFunnome() {
+		return funnome;
+	}
+
+	public void setFunnome(String funnome) {
+		this.funnome = funnome;
+	}
+
+	public LocalDate getFunnascimento() {
+		return funnascimento;
+	}
+
+	public void setFunnascimento(LocalDate funnascimento) {
+		this.funnascimento = funnascimento;
+	}
+
+	public Long getFunsalario() {
+		return funsalario;
+	}
+
+	public void setFunsalario(Long funsalario) {
+		this.funsalario = funsalario;
+	}
+
+	// inserir os metodos get e o set
+	public Departamento getDepartamento() {
+		return departamento;
+	}
+
+	public void setDepartamento(Departamento departamento) {
+		this.departamento = departamento;
+	}
+}
